1fc60310f250f2f18e81cdf29a5b083562da427c
tzahy92 <tzahy92@gmail.com>
2022-03-17 12:01:14 +0200

    Initial commit

---
+++ src/main/java/com/lotto/guess_lotto_number/GuessLottoNumberApplication.java
@@ -0,0 +1,13 @@
+package com.lotto.guess_lotto_number;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessLottoNumberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuessLottoNumberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lotto/guess_lotto_number/GuessLottoNumberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lotto.guess_lotto_number;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuessLottoNumberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
